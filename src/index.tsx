87866f7615cd72ec3b5473c7d9ae9143b33eb690
Nikolay Yurikov <nyurikov@gmail.com>
2023-05-04 18:18:18 +0500

    done mvp

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client';
+import { router } from './router'
+import { RouterProvider } from 'react-router-dom';
+import { client } from './api/Apollo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
